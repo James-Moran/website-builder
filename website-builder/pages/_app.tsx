063dff4eb685292a24631b3e1a98a39323eefee3
James Moran <james.bc.moran@gmail.com>
2022-01-18 17:11:06 +0000

    Added Head settings and font to app

--- website-builder/pages/_app.tsx
+++ website-builder/pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Website Builder</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:300,400,600"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
