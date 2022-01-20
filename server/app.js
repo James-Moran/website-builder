0736e4bac69b8d2219a51a5fdee312e13338c73e
James Moran <james.bc.moran@gmail.com>
2022-01-20 14:56:38 +0000

    Added more authentication methods

--- server/app.js
+++ server/app.js
@@ -1,32 +1,46 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const passport = require('passport');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 var app = express();
 
-require('./config/database.js');
+require("./config/database.js");
 
-require('./models/user');
-require('./models/shop');
+require("./models/user");
+require("./models/shop");
 
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 app.use(passport.initialize());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+const whitelist = ["http://localhost:3000", "http://localhost:8000"];
+
+var corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    console.log(origin);
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 /**
  * -------------- ROUTES ----------------
  */
-app.use(require('./routes'));
-
+app.use(require("./routes"));
 
 /**
  * -------------- SERVER ----------------
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,38 +1,65 @@
-const mongoose = require('mongoose');
-const router = require('express').Router();
-const User = mongoose.model('User');
-const passport = require('passport');
-const utils = require('../lib/utils');
+const mongoose = require("mongoose");
+const router = require("express").Router();
+const User = mongoose.model("User");
+const passport = require("passport");
+const utils = require("../lib/utils");
 
-router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  res.status(200).json({ success: true, msg: "You are successfully authenticated to this route!" });
+router.get(
+  "/checklogin",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    console.log("Checking Login");
+    res.status(200).json({
+      success: true,
+    });
+  }
+);
+
+router.get("/logout", function (req, res, next) {
+  console.log("Attempt Logout");
+  res.clearCookie("wbjwt");
+  res.status(200).json({
+    success: true,
+  });
 });
 
 // Validate an existing user and issue a JWT
-router.post('/login', function (req, res, next) {
-
+router.post("/login", function (req, res, next) {
+  console.log("Attempt Login");
   User.findOne({ username: req.body.username })
     .then((user) => {
-
       if (!user) {
-        return res.status(401).json({ success: false, msg: "could not find user" });
+        return res
+          .status(401)
+          .json({ success: false, msg: "could not find user" });
       }
 
       // Function defined at bottom of app.js
-      const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+      const isValid = utils.validPassword(
+        req.body.password,
+        user.hash,
+        user.salt
+      );
 
       if (isValid) {
-
         const tokenObject = utils.issueJWT(user);
+        console.log("return cookie");
+        res.cookie("wbjwt", tokenObject.token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        });
 
-        res.status(200).json({ success: true, token: tokenObject.token, expiresIn: tokenObject.expires });
-
+        res.status(200).json({
+          success: true,
+          token: tokenObject.token,
+          expiresIn: tokenObject.expires,
+        });
       } else {
-
-        res.status(401).json({ success: false, msg: "you entered the wrong password" });
-
+        res
+          .status(401)
+          .json({ success: false, msg: "you entered the wrong password" });
       }
-
     })
     .catch((err) => {
       next(err);
@@ -40,9 +67,10 @@ router.post('/login', function (req, res, next) {
 });
 
 // Register a new user
-router.post('/register', function (req, res, next) {
+router.post("/register", function (req, res, next) {
   const password = req.body.password;
   const username = req.body.username;
+
   const saltHash = utils.genPassword(password);
 
   const salt = saltHash.salt;
@@ -51,22 +79,30 @@ router.post('/register', function (req, res, next) {
   const newUser = new User({
     username: username,
     hash: hash,
-    salt: salt
+    salt: salt,
   });
 
+  // Find if email has previously been used
+
   try {
-
-    newUser.save()
-      .then((user) => {
-        res.json({ success: true });
+    newUser.save().then((user) => {
+      const tokenObject = utils.issueJWT(user);
+      console.log("return cookie");
+      res.cookie("wbjwt", tokenObject.token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
       });
-
+      res.status(200).json({
+        success: true,
+        token: tokenObject.token,
+        expiresIn: tokenObject.expires,
+        user: user,
+      });
+    });
   } catch (err) {
-
     res.json({ success: false, msg: err });
-
   }
-
 });
 
 module.exports = router;
